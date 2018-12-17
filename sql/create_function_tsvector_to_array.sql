aeafe5813df963ddef3c35f99ff104969c182395
Daniel Small <piktur.io@gmail.com>
2018-12-18 05:42:03 +1100

    Replace example with the given argument

--- sql/create_function_tsvector_to_array.sql
+++ sql/create_function_tsvector_to_array.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION tsvector_to_array(tsvector)
 RETURNS text[] IMMUTABLE AS $$
 BEGIN
-  RETURN array_agg(word) FROM ts_stat('SELECT (' || quote_literal(to_tsvector('Eaton Vance')) || ')::tsvector');
+  RETURN array_agg(word) FROM ts_stat('SELECT (' || quote_literal($1) || ')::tsvector');
 END
 $$ LANGUAGE plpgsql;
