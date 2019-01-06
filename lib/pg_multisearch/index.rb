ced57ad99534d8630bb6bcf53588e8b0a2d638e1
Daniel Small <piktur.io@gmail.com>
2019-01-07 04:57:47 +1100

    Nest base model within Index namespace

--- lib/pg_multisearch/index.rb
+++ lib/pg_multisearch/index.rb
@@ -1,42 +1,22 @@
 # frozen_string_literal: true
 
 module PgMultisearch
-  class Index < ::ActiveRecord::Base
+  module Index
     extend ::ActiveSupport::Autoload
 
-    autoload :Builder
-    autoload :Loader
-    autoload :Pagination
-    autoload :Preloader
+    autoload :Base
+    autoload :Cache
+    autoload :ClassMethods
+    autoload :InstanceMethods
+    autoload :Meta
     autoload :Rebuild
     autoload :Rebuilder
+    autoload :Relation
+    autoload :Scopes
 
-    CONTENT_COLUMN = 'content'
-    HEADER_COLUMN = 'header'
-    DMETAPHONE_COLUMN = 'dmetaphone'
-
-    include ::PgSearch
-
-    self.table_name = 'pg_search_documents'
-
-    # @!attribute [r] searchable
-    #   @return [ActiveRecord::Base]
-    belongs_to  :searchable,
-                polymorphic: true,
-                inverse_of:  :pg_search_document
-
-    require_relative './index/scope.rb'
-    extend Scope
-
-    # @return [Document::Base]
-    def to_document
-      ::Object.const_get(searchable_type, false).to_document(data || EMPTY_HASH, rank)
+    # @return [Meta]
+    def self.meta
+      Base.meta
     end
-
-    # @return [Float]
-    def pg_search_rank
-      self[:pg_search_rank]
-    end
-    alias rank pg_search_rank
   end
 end
