5dde5699559d53ca4d78efe0c3b0ec4e74e59785
Daniel Small <piktur.io@gmail.com>
2019-01-14 22:06:03 +1100

    Coalesce NULL values

--- sql/create_function_tsvector_to_array.sql
+++ sql/create_function_tsvector_to_array.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION tsvector_to_array(tsvector)
 RETURNS text[] PARALLEL SAFE STRICT AS $$
 BEGIN
-  RETURN array_agg(word) FROM ts_stat('SELECT (' || quote_literal($1) || ')::tsvector');
+  RETURN array_agg(word) FROM ts_stat('SELECT (' || quote_literal(coalesce($1, '')) || ')::tsvector');
 END
 $$ LANGUAGE plpgsql;
